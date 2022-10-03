2ef763889824edb890aa74c8d216360e53d5426b
Zak Brown <96209266+ZakB659@users.noreply.github.com>
2022-10-03 15:55:52 +0100

    Update Charachter.cs

--- Charachter.cs
+++ Charachter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +21,9 @@ namespace Computer_Science_end_project
 
     }
 
+   
+
+
     class Charachter
     {
         private Vector2 Location;
@@ -31,6 +34,8 @@ namespace Computer_Science_end_project
         private int MovementSpeed;
         private int windowWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
         private int windowHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+        private int windowWidthdiv3 = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width/3;
+        private int windowHeightdiv2 = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height/2;
 
         public Texture2D _Texture { get => Texture; set => Texture = value; }
         public int _Size { get => size; set => size = value; }
@@ -40,6 +45,8 @@ namespace Computer_Science_end_project
         public Rectangle _HitBox { get => HitBox; set => HitBox = value; }
         public int WindowWidth { get => windowWidth; set => windowWidth = value; }
         public int WindowHeight { get => windowHeight; set => windowHeight = value; }
+        public int WindowWidthdiv3 { get => windowWidthdiv3; set => windowWidthdiv3 = value; }
+        public int WindowHeightdiv2 { get => windowHeightdiv2; set => windowHeightdiv2 = value; }
 
         public virtual void loadcontent(ContentManager content,string name)
         {
